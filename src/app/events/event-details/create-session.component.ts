f58e153a29febe43bb9b4f830a867632853d07fa
ifycode <marychord@gmail.com>
2021-01-03 04:03:36 -0800

    Creating custom validators - 2

--- src/app/events/event-details/create-session.component.ts
+++ src/app/events/event-details/create-session.component.ts
@@ -48,7 +48,7 @@ export class CreateSessionComponent implements OnInit {
     this.presenter = new FormControl('', Validators.required);
     this.duration = new FormControl('', Validators.required);
     this.level = new FormControl('', Validators.required);
-    this.abstract = new FormControl('', [Validators.required, Validators.maxLength(400), this.restrictedWords]);
+    this.abstract = new FormControl('', [Validators.required, Validators.maxLength(400), this.restrictedWords(['foo', 'bar'])]);
     
     this.newSessionForm = new FormGroup({
       name: this.name,
@@ -59,10 +59,18 @@ export class CreateSessionComponent implements OnInit {
     });
   }
 
-  private restrictedWords(control: FormControl): {[key: string]: any} {
-    return control.value.includes('foo')
-      ? {'restrictedWords': 'foo'}
-      : null;
+  private restrictedWords(words) {
+    return (control: FormControl): {[key: string]: any} =>  {
+      if (!words) return null;
+
+      let invalidWords = words
+        .map(w => control.value.includes(w) ? w : null)
+        .filter(w => w != null);
+
+      return invalidWords && invalidWords.length > 0
+        ? {'restrictedWords': invalidWords.join(', ')}
+        : null;
+    }
   }
 
   saveSession(formValues) {
