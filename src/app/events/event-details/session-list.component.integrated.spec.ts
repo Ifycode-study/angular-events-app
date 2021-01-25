f4c25c5d75b306dcdb43efacdee797ea3d253605
ifycode <marychord@gmail.com>
2021-01-24 18:35:27 -0800

    Testing components with deep integrated tests (session-list con'td)

--- src/app/events/event-details/session-list.component.integrated.spec.ts
+++ src/app/events/event-details/session-list.component.integrated.spec.ts
@@ -3,6 +3,8 @@ import { DebugElement } from "@angular/core";
 import { SessionListComponent } from "./session-list.component";
 import { AuthService } from "src/app/user/auth.service";
 import { VoterService } from "./voter.service";
+import { DurationPipe } from "../shared";
+import { CollapsibleWellComponent } from "src/app/common";
 //import Issession & By later when you need them
 
 describe('SessionListComponent', () => {
@@ -18,7 +20,9 @@ describe('SessionListComponent', () => {
     TestBed.configureTestingModule({
       imports: [],
       declarations: [
-        SessionListComponent
+        SessionListComponent,
+        DurationPipe,
+        CollapsibleWellComponent
       ],
       providers: [
         { provide: AuthService, useValue: mockAuthService },
@@ -33,5 +37,30 @@ describe('SessionListComponent', () => {
     component = fixture.componentInstance;
     debugEl = fixture.debugElement;
     element = fixture.nativeElement;
+  });
+
+  describe('Initial display', () => {
+    it('should have the correct session title', () => {
+      component.sessions = [
+        {
+          id: 3,
+          name: 'Session 1',
+          presenter: 'Joe',
+          duration: 1,
+          level: 'beginner',
+          abstract: 'abstract',
+          voters: ['john', 'bob']
+        }
+      ];
+      component.filterBy = 'all';
+      component.sortBy = 'name';
+      component.eventId = 4;
+
+      component.ngOnChanges();
+
+      fixture.detectChanges();
+
+      expect(element.querySelector('[well-title]').textContent).toContain('Session 1');
+    });
   });
 });
